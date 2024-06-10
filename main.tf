3475d74209722d04f4044c0e126af89321317df1
Mike Wies <mike.wies@snowflake.com>
2024-06-09 21:11:42 -0400

    Update main.tf
    corrected naming

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db_tf" {
-  name    = "DEMO_DB_tf"
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB_TF"
   comment = "Database for Snowflake Terraform demo"
 }
 
