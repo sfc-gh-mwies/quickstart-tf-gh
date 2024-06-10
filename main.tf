cd96e4e6a9d8696573090f855ebb3361f8abf395
Mike Wies <mike.wies@snowflake.com>
2024-06-09 21:06:36 -0400

    Update main.tf
    Altered db name to a db which does not exists

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db" {
-  name    = "DEMO_DB"
+resource "snowflake_database" "demo_db_tf" {
+  name    = "DEMO_DB_tf"
   comment = "Database for Snowflake Terraform demo"
 }
